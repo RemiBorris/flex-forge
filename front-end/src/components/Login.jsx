e4db34cb8e64ed8ca996364430b9f976c6288289
Kyle Lindsay <kyle.lindsay333@gmail.com>
2024-11-17 11:34:42 -0800

    create front end, implement login functionality

---
+++ front-end/src/components/Login.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import './Login.css';
+
+/*
+use useState to manage username input. When a user types into the input, the
+username value updates
+*/
+const Login = () => {
+  const [username, setUsername] = useState('');
+
+  const handleLogin = () => {
+    axios.get(`${process.env.REACT_APP_API_URL}/users`)
+    .then(response => {
+      // Search for a user with a matching name
+      const user = response.data.find(user => user.name === username);
+      if (user) {
+        // Save the user's ID in localStorage
+        localStorage.setItem('userId', user.id);
+        alert(`Welcome, ${user.name}! Your User ID is: ${user.id}`);
+      } else {
+        // No matching user found
+        alert('Invalid username!');
+      }
+    })
+    .catch(() => alert('Error connecting to the server.'));
+  };
+
+  const handleLogout = () => {
+    localStorage.removeItem('userId');
+    alert('user logged out');
+  }
+
+  return (
+    <div className="main-container">
+      <header>
+        <h1>Flex Forge</h1>
+      </header>
+      <div className="login-form">
+        <input
+          className="input-field"
+          type="text"
+          placeholder="Enter Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input className="input-field" placeholder="Enter Password" />
+        <button className="login-btn" onClick={handleLogin}>Login</button>
+        <button className="logout-btn" onClick={handleLogout}>Logout</button>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
