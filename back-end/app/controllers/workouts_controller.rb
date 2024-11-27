81fac5979eb7408e512a068d9874ae00b063206c
Daman Bual <damanbual@gmail.com>
2024-11-27 12:24:27 -0800

    edit features fixing

--- back-end/app/controllers/workouts_controller.rb
+++ back-end/app/controllers/workouts_controller.rb
@@ -1,26 +1,25 @@
 class WorkoutsController < ApplicationController
-
   before_action :set_user
-  before_action :set_workout, only: [:show, :update, :destroy] #excludes index
+  before_action :set_workout, only: [:show, :update, :destroy]
 
   def index
-    workouts = @user.workouts.includes(workout_exercises: [:exercise, :set_entries]) # Include set_entries
+    workouts = @user.workouts.includes(workout_exercises: [:exercise, :set_entries])
     render json: workouts
   end
 
   def show
-    render json: @workout.as_json(include: { 
-      workout_exercises: { 
+    render json: @workout.as_json(include: {
+      workout_exercises: {
         include: [
-          :exercise, 
-          :set_entries # Include set_entries in the response for show
-        ] 
-      } 
+          :exercise,
+          :set_entries
+        ]
+      }
     })
   end
 
   def create
-    workout = Workout.create(workout_params)
+    workout = @user.workouts.new(workout_params)
     if workout.save
       render json: workout, status: :created
     else
@@ -29,24 +28,59 @@ class WorkoutsController < ApplicationController
   end
 
   def update
-    workout = Workout.find(params[:id])
-    if workout.update(workout_params)
-      render json: workout
+    sanitized_params = sanitize_params_for_update
+    if @workout.update(sanitized_params)
+      render json: @workout.as_json(include: {
+        workout_exercises: {
+          include: [
+            :exercise,
+            :set_entries
+          ]
+        }
+      })
     else
-      render json: workout.errors, status: :unprocessable_entity
+      render json: @workout.errors, status: :unprocessable_entity
     end
   end
 
-  def destroy 
-    workout = Workout.find(params[:id])
-    workout.destroy
+  def destroy
+    @workout.destroy
     head :no_content
   end
 
   private
-  
+
   def workout_params
-    params.require(:workout).permit(:user_id, :date, :notes)
+    params.require(:workout).permit(
+      :date,
+      :notes,
+      workout_exercises_attributes: [
+        :id,
+        :exercise_id,
+        :_destroy,
+        set_entries_attributes: [
+          :id,
+          :set_number,
+          :reps,
+          :weight,
+          :_destroy
+        ]
+      ]
+    )
+  end
+
+  # Sanitize params by removing unnecessary keys
+  def sanitize_params_for_update
+    sanitized = workout_params.to_h
+    sanitized.delete('id') # Ensure 'id' isn't passed to the top-level workout object
+    sanitized.deep_transform_values do |value|
+      # Remove nested `created_at`, `updated_at` where unnecessary
+      if value.is_a?(Hash)
+        value.reject { |k, _| %w[created_at updated_at].include?(k) }
+      else
+        value
+      end
+    end
   end
 
   def set_user
@@ -56,5 +90,4 @@ class WorkoutsController < ApplicationController
   def set_workout
     @workout = @user.workouts.find(params[:id])
   end
-  
 end
--- back-end/app/models/workout.rb
+++ back-end/app/models/workout.rb
@@ -1,4 +1,5 @@
 class Workout < ApplicationRecord
   belongs_to :user
   has_many :workout_exercises, dependent: :destroy
+  accepts_nested_attributes_for :workout_exercises, allow_destroy: true # Enables nested attributes for workout_exercises
 end
