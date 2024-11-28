65e8140f056de853dd8d0a4adc4025500562ad59
remiborris <remiborris@gmail.com>
2024-11-27 22:10:07 -0400

    added images folder to house all the avatars and group them in a variable with proper params

---
+++ front-end/src/images/avatars.js
@@ -0,0 +1,40 @@
+const avatar1 = {
+  "id": "1",
+  "url": `${process.env.PUBLIC_URL}/avatar1.png`
+}
+const avatar2 = {
+  "id": "2",
+  "url": `${process.env.PUBLIC_URL}/avatar2.png`
+}
+const avatar3 = {
+  "id": "3",
+  "url": `${process.env.PUBLIC_URL}/avatar3.png`
+}
+const avatar4 = {
+  "id": "4",
+  "url": `${process.env.PUBLIC_URL}/avatar4.png`
+}
+const avatar5 = {
+  "id": "5",
+  "url": `${process.env.PUBLIC_URL}/avatar5.png`
+}
+const avatar6 = {
+  "id": "6",
+  "url": `${process.env.PUBLIC_URL}/avatar6.png`
+}
+const avatar7 = {
+  "id": "7",
+  "url": `${process.env.PUBLIC_URL}/avatar7.png`
+}
+
+const avatars = [
+  avatar1,
+  avatar2,
+  avatar3,
+  avatar4,
+  avatar5,
+  avatar6,
+  avatar7
+]
+
+export default avatars
